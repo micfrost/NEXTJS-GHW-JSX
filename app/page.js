4168d1d3d5ca8756f31bd237b32a98265ee166b2
Michal Frost <116974794+micfrost@users.noreply.github.com>
2024-10-13 14:12:35 +0200

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Hello World, Greetings from Michal Frost.</h1>
+            <p>Welcome to the Next.js World</p>
+        </div>
+
+    );
+}
